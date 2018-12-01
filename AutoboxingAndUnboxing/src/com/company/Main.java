f4ef22fddf486bb829209212a0c77e3dae26f781
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-01 10:12:10 +0200

    . . . Lecture 89

--- AutoboxingAndUnboxing/src/com/company/Main.java
+++ AutoboxingAndUnboxing/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.security.interfaces.DSAPublicKey;
 import java.util.ArrayList;
 
 class IntClass {
@@ -32,6 +33,30 @@ public class Main {
 //        ArrayList<int> intArrayList = new ArrayList<int>();
         ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
         intClassArrayList.add(new IntClass(54));
-        
+
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(43.22);
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        for (int i = 0; i <= 10 ; i++) {
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(i + " ->> " + intArrayList.get(i).intValue());
+        }
+
+        Integer myIntValue = 56;//Integer.valueOf(56);
+        int myInt = myIntValue; // myIntValue.intValue();
+
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for (double dbl = 0.0; dbl <= 10.0; dbl+=0.5) {
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+
+        for (int i = 0; i < myDoubleValues.size(); i++) {
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " ------>" + value);
+        }
     }
 }
