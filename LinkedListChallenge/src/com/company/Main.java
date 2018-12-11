77c20588db3f584df3d3a9db19be6226cf8c7c08
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-12 00:15:43 +0200

    . . .

--- LinkedListChallenge/src/com/company/Main.java
+++ LinkedListChallenge/src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Main {
 
@@ -47,14 +45,96 @@ public class Main {
     }
 
     private static void play(LinkedList<Song> playList){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean forward = true;
         ListIterator<Song> listIterator = playList.listIterator();
         if(playList.size() == 0){
             System.out.println("No songs in playlist.");
             return;
         }else{
             System.out.println("Now playying " + listIterator.next().toString());
-
+            printMenu();
         }
+
+        while(!quit){
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(action){
+                case 0:
+                    System.out.println("Playlist complete.");
+                    quit = true;
+                    break;
+                case 1:
+                    if(!forward){
+                        if(listIterator.hasNext()){
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if(listIterator.hasNext()){
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    }else{
+                        System.out.println("We have reached the end of playlist");
+                        forward = false;
+                    }
+                    break;
+                case 2:
+                    if(forward){
+                        if(listIterator.hasPrevious()){
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+                    if(listIterator.hasPrevious()){
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    }else{
+                        System.out.println("We are at the start of the playlist");
+                    }
+                    break;
+                case 3:
+                    if(forward){
+                        if(listIterator.hasPrevious()){
+                            System.out.println("Now replaing " + listIterator.previous().toString());
+                        }else{
+                            System.out.println("Where at the start of the list");
+                        }
+                    }else{
+                        if(listIterator.hasNext()){
+                            System.out.println("Now replaying" + listIterator.next().toString());
+                            forward = true;
+                        }else{
+                            System.out.println("We have reached the end of the list");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playList);
+                case 5:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu(){
+        System.out.println("Available actions: \npress");
+        System.out.println("0 - to quit\n" +
+                "1 - to play next song\n" +
+                "2 - to play previous song\n" +
+                "3 - to replay the current song\n" +
+                "4 - list songs in the playlist\n" +
+                "5 - print available actions.");
+    }
+
+    private static void printList(LinkedList<Song> playlist){
+        Iterator<Song> iterator = playlist.iterator();
+        System.out.println("===============================");
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("===============================");
     }
 
 
