6e9cb8a5ce0dc023d1ccbaf4037e68f7cfb1b2b7
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-13 00:58:14 +0200

    InnerClassesGear

--- InnerClasses/src/com/company/Main.java
+++ InnerClasses/src/com/company/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        GearBox mcLaren = new GearBox(6);
+        GearBox.Gear first = mcLaren.new Gear(1, 12.3);
 
+        System.out.println(first.driveSpeed(1000));
 
     }
 }
