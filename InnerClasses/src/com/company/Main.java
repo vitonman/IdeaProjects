4208824a7a0dc749e2769c9ea34eba6e07cd4f9f
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-13 03:14:26 +0200

    button

--- InnerClasses/src/com/company/Main.java
+++ InnerClasses/src/com/company/Main.java
@@ -1,13 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("print")
 
     public static void main(String[] args) {
 	// write your code here
-        GearBox mcLaren = new GearBox(6);
-        GearBox.Gear first = mcLaren.new Gear(1, 12.3);
-
-        System.out.println(first.driveSpeed(1000));
-
+//        GearBox mcLaren = new GearBox(6);
+//        mcLaren.addGear(1, 5.3);
+//        mcLaren.addGear(2, 10.6);
+//        mcLaren.addGear(3, 15.9);
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(1);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(1000));
+//        mcLaren.changeGear(2);
+//        System.out.println(mcLaren.wheelSpeed(3000));
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(3);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(6000));
     }
 }
