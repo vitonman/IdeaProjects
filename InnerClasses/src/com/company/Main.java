1dd2d84e99147a236ce8746bc59e41271212d193
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-13 03:26:08 +0200

    InnerClassChallenge

--- InnerClasses/src/com/company/Main.java
+++ InnerClasses/src/com/company/Main.java
@@ -4,23 +4,43 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-    private static Button btnPrint = new Button("print")
+    private static Button btnPrint = new Button("print");
 
     public static void main(String[] args) {
-	// write your code here
-//        GearBox mcLaren = new GearBox(6);
-//        mcLaren.addGear(1, 5.3);
-//        mcLaren.addGear(2, 10.6);
-//        mcLaren.addGear(3, 15.9);
-//        mcLaren.operateClutch(true);
-//        mcLaren.changeGear(1);
-//        mcLaren.operateClutch(false);
-//        System.out.println(mcLaren.wheelSpeed(1000));
-//        mcLaren.changeGear(2);
-//        System.out.println(mcLaren.wheelSpeed(3000));
-//        mcLaren.operateClutch(true);
-//        mcLaren.changeGear(3);
-//        mcLaren.operateClutch(false);
-//        System.out.println(mcLaren.wheelSpeed(6000));
+
+//        class ClickListner implements Button.OnClickListener {
+//            public ClickListner(){
+//                System.out.println("I've been attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//
+//        btnPrint.setOnClickListener(new ClickListner());
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+    }
+
+    private static void  listen(){
+        boolean quit = false;
+        while(!quit){
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 }
