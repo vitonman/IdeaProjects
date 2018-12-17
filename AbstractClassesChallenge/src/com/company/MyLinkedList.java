3c5b164f7c06a45844da23ba03ae4e6db51fb4e6
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-17 18:57:23 +0200

    . . .

--- AbstractClassesChallenge/src/com/company/MyLinkedList.java
+++ AbstractClassesChallenge/src/com/company/MyLinkedList.java
@@ -30,27 +30,25 @@ public class MyLinkedList implements NodeList {
                     currentItem = currentItem.next();
                 }else{
                     //there os no next, so insert at end of list
-                    currentItem.setNext(newItem);
-                    newItem.setPrevious(currentItem);
+                    currentItem.setNext(newItem).setPrevious(currentItem);
                     return true;
                 }
             }else if(comparison>0){
                 //newItem is less, insert before
                 if(currentItem.previous() != null){
-                    currentItem.previous().setNext(newItem);
-                    newItem.setPrevious(currentItem.previous());
-                    newItem.setNext(currentItem);
-                    currentItem.setPrevious(newItem);
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
                 }else{
                     //the node with previous is the root
-                    newItem.setNext(this.root);
-                    this.root.setPrevious(newItem);
+                    newItem.setNext(this.root).setPrevious(newItem);
+                    this.root = newItem;
                 }
+                return true;
             }else {
                 //equal
                 System.out.println(newItem.getValue() + " is already present, not added.");
                 return false;
-            }
+            }//17:00
         }
         return false;
     }
