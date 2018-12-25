9ab639459d72ab6c08f807d7ee7ac2cc80caf17e
vitodon <45338513+vitonman@users.noreply.github.com>
2018-12-25 16:29:18 +0200

    generics 3

--- Generics/src/com/company/Main.java
+++ Generics/src/com/company/Main.java
@@ -6,22 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-        //items.add("tim");
-        items.add(4);
-        items.add(5);
-        items.add(6);
+//        ArrayList<Integer> items = new ArrayList<>();
+//        items.add(1);
+//        items.add(2);
+//        items.add(3);
+//        //items.add("tim");
+//        items.add(4);
+//        items.add(5);
+//        items.add(6);
+//
+//        printDoubled(items);
+//
+//    }
+//
+//    private static void printDoubled(ArrayList<Integer> n){
+//        for(int i : n){
+//            System.out.println(i * 2);
+//        }
+//    }
 
-        printDoubled(items);
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer backgam = new SoccerPlayer("Beckham");
 
-    }
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(backgam);
 
-    private static void printDoubled(ArrayList<Integer> n){
-        for(int i : n){
-            System.out.println(i * 2);
-        }
-    }
 }
